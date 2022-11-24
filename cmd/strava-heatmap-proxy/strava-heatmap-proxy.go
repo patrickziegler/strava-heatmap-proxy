c45f1323c59c6f44546d8c8c90189243b744ca9d
Patrick Ziegler <zipat@proton.me>
2022-11-24 10:01:20 +0100

    Minor code style improvement

--- cmd/strava-heatmap-proxy/strava-heatmap-proxy.go
+++ cmd/strava-heatmap-proxy/strava-heatmap-proxy.go
@@ -27,21 +27,27 @@ func ParseConfig(path string) (*Config, error) {
 	if err != nil {
 		return nil, fmt.Errorf("Could not open config file '%s': %w", path, err)
 	}
+
 	body, err := io.ReadAll(file)
 	if err != nil {
 		return nil, fmt.Errorf("Could not read from config file '%s': %w", path, err)
 	}
+
 	var config Config
+
 	err = json.Unmarshal(body, &config)
 	if err != nil {
 		return nil, fmt.Errorf("Could not unmarshal json from config file '%s': %w", path, err)
 	}
+
 	if config.Email == "" {
 		return nil, fmt.Errorf("Mandatory field 'Email' not found in '%s'", path)
 	}
+
 	if config.Password == "" {
 		return nil, fmt.Errorf("Mandatory field 'Password' not found in '%s'", path)
 	}
+
 	return &config, nil
 }
 
@@ -56,14 +62,17 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to get configuration: %s", err)
 	}
+
 	client := strava.NewStravaClient()
 
 	if err = client.Authenticate(config.Email, config.Password); err != nil {
 		log.Fatalf("Failed to authenticate client: %s", err)
 	}
+
 	for k, v := range client.GetCloudFrontCookies() {
 		fmt.Printf("%s\t%s\n", k, v)
 	}
+
 	log.Printf("Starting heatmap proxy on port %s ..", *param.Port)
 
 	http.Handle("/", strava.NewStravaProxy(client))
