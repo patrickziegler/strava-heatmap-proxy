4d7dfdd94c730ec7a9a7efc0057176d7bc341716
Patrick Ziegler <zipat@proton.me>
2023-03-23 10:38:12 +0100

    Code style improvement

--- internal/strava/config.go
+++ internal/strava/config.go
@@ -17,26 +17,21 @@ func ParseConfig(path string) (*Config, error) {
 	if err != nil {
 		return nil, fmt.Errorf("Could not open config file '%s': %w", path, err)
 	}
-
+	defer file.Close()
 	body, err := io.ReadAll(file)
 	if err != nil {
 		return nil, fmt.Errorf("Could not read from config file '%s': %w", path, err)
 	}
-
 	var config Config
-
 	err = json.Unmarshal(body, &config)
 	if err != nil {
 		return nil, fmt.Errorf("Could not unmarshal json from config file '%s': %w", path, err)
 	}
-
 	if config.Email == "" {
 		return nil, fmt.Errorf("Mandatory field 'Email' not found in '%s'", path)
 	}
-
 	if config.Password == "" {
 		return nil, fmt.Errorf("Mandatory field 'Password' not found in '%s'", path)
 	}
-
 	return &config, nil
 }
