aee5064cb4d3b2f7b483001cc5f713c29aa07de3
Patrick Ziegler <zipat@proton.me>
2025-01-30 11:05:43 +0100

    Lower case error strings

--- internal/strava/config.go
+++ internal/strava/config.go
@@ -15,23 +15,23 @@ type StravaConfig struct {
 func ParseConfig(path string) (*StravaConfig, error) {
 	file, err := os.Open(path)
 	if err != nil {
-		return nil, fmt.Errorf("Could not open config file '%s': %w", path, err)
+		return nil, fmt.Errorf("could not open config file '%s': %w", path, err)
 	}
 	defer file.Close()
 	body, err := io.ReadAll(file)
 	if err != nil {
-		return nil, fmt.Errorf("Could not read from config file '%s': %w", path, err)
+		return nil, fmt.Errorf("could not read from config file '%s': %w", path, err)
 	}
 	var config StravaConfig
 	err = json.Unmarshal(body, &config)
 	if err != nil {
-		return nil, fmt.Errorf("Could not unmarshal json from config file '%s': %w", path, err)
+		return nil, fmt.Errorf("could not unmarshal json from config file '%s': %w", path, err)
 	}
 	if config.Email == "" {
-		return nil, fmt.Errorf("Mandatory field 'Email' not found in '%s'", path)
+		return nil, fmt.Errorf("mandatory field 'Email' not found in '%s'", path)
 	}
 	if config.Password == "" {
-		return nil, fmt.Errorf("Mandatory field 'Password' not found in '%s'", path)
+		return nil, fmt.Errorf("mandatory field 'Password' not found in '%s'", path)
 	}
 	return &config, nil
 }
